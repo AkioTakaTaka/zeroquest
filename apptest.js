ce138bdc9a2a51cc336c1849285502f3f715ff80
Akio <alex.pos1tive@icloud.com>
2023-09-03 17:50:40 +0300

    Adding code

---
+++ apptest.js
@@ -0,0 +1,20 @@
+//console.log(a.toString().length)
+
+// for (let i = 0; i < a.toString().length; i++){
+// z += '0'
+// }
+// console.log(z + a) РАБОТАЕТ!!!!
+    
+    const inputOne = document.getElementById('input1')
+    const inputTwo = document.getElementById('input2')
+    const outputBtn = document.getElementById('output')
+    const resultArea = document.getElementById('result')
+
+    outputBtn.onclick = function sum(){
+        if (inputOne.value.length < inputTwo.value) {
+
+            inputOne.value = '0' + inputOne.value
+        }
+        
+        resultArea.textContent = inputOne.value
+}
